c31a1b32a205d051304127752c2d438430ceabba
J3C118045 <72448563+J3C118045@users.noreply.github.com>
2020-10-12 20:02:18 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $host   = "sql113.epizy.com";
+    $user   = "epiz_26776644";
+    $psw    = "sTucfA0iVrvx4Z";
+    $db_name= "epiz_26776644_pabw";
+    $kon = mysqli_connect($host,$user,$psw,$db_name);
+
+?>
